a0ab9caa60bba5e4f01ecb71e15c9cee380bc850
Falak <falak.raza.butt@gmail.com>
2019-11-02 17:05:27 +0100

    in seed file account is linked with transfer and navigation bar added

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,4 @@
 class Transfer < ApplicationRecord
   has_and_belongs_to_many :users, :join_table => "transfers_users"
+  has_and_belongs_to_many :accounts, :join_table => "accounts_transfers"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 u1=User.create email: 'user_a@fast.com', password: 'abc123', password_confirmation: 'abc123',name: 'User A'
 t1=Transfer.create value: 1000.0, transaction_status: true
-Account.create balance: 1000.0, user_id: u1.id, currency_type: 'usd'
+a1 = Account.create balance: 1000.0, user_id: u1.id, currency_type: 'usd'
 u1.transfers << t1
+a1.transfers << t1
 
 
 u2=User.create email: 'user_2@fast.com', password: 'abc123', password_confirmation: 'abc123',name: 'User B'
 t2=Transfer.create value: 1000.0, transaction_status: true
-Account.create balance: 1000.0, user_id: u2.id, currency_type: 'usd'
+a2=Account.create balance: 1000.0, user_id: u2.id, currency_type: 'usd'
 u2.transfers << t2
+a2.transfers << t2
